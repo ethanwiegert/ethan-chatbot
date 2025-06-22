7dfa345c15668fef9da5564aa80a545906c985ca
Ethan Wiegert <ewiegert99@gmail.com>
2025-06-22 10:13:31 -0400

    Initial design with required files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useChat } from '@ai-sdk/react';
+
 import {
   Card,
   CardAction,
@@ -9,26 +13,67 @@ import {
 } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
 
-export default function Home() {
+
+/*
+left to implement (for initial functionality):
+-chat setup and instructions
+-linkedin pdf and text file summarizing some of my personal projects (do a google doc so it's easy to maintain)
+-testing
+-improving mobile design and functionality (utilize tailwind breakpoints)
+
+future
+-adding in more 'tools' for the ai
+  *sending me an email of a question that it was unable to answer
+*/
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+
+  async function Test(){
+    console.log("test")
+  }
+
   return (
-    <div className="flex-col justify-between items-center h-screen">
-      <div className="row m-10">
-        <Card>
+    <div className="flex flex-col m-8 w-11/12-md py-12 ">
+      {messages.length<1 ?         
+      <Card>
           <CardHeader>
-            <CardTitle>Ethan Wiegert</CardTitle>
-            <CardDescription>Ask me anything</CardDescription>
+            <CardTitle>"EthanBot"</CardTitle>
+            <CardDescription>As if you were talking to me</CardDescription>
           </CardHeader>
           <CardContent>
-            <p>A chat bot as if you were talking to me.  Focused on my background and personal projects.</p>
+            <p>An AI model trained on my qualifications and background, that is eager to answer your questions! </p>
+            <br/>
+            <p>Technologies - Next.js, shadcn, Vercel AI SDK, Open AI</p>
           </CardContent>
           <CardFooter>
-            <p>Thank you for visiting my website!</p>
+            <p>Thanks for visiting!</p>
           </CardFooter>
-        </Card>
-      </div>
-      <div className="row m-10">
-        <Textarea placeholder="Ask me anything here..." className="w-full" />
-      </div>
+        </Card> : null}
+      {messages.map(message => (
+        <div key={message.id} className="">
+          {message.role === 'user' ? <b>You</b> : <b>EthanBot</b>}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case 'text':
+                return <div key={`${message.id}-${i}`} className="py-4"><Card><CardContent>{part.text}</CardContent></Card></div>;
+            }
+          })}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 p-6 m-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl w-11/12"
+          value={input}
+          placeholder="Ask me anything regarding Ethan Wiegert's background and work."
+          onChange={handleInputChange}
+          maxLength={200}
+        />
+      </form>
+      <br/>
+      <button onClick={Test}>Test</button>
     </div>
   );
 }
