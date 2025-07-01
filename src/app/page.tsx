5ff46dda19d4e0a26ad328f5acbedd08f92abd85
Ethan Wiegert <ewiegert99@gmail.com>
2025-07-01 13:34:48 -0400

    Text size screen size responsive, scroll focused with chat.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,14 @@ import {
 } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
 import { ModeToggle } from "@/components/ui/mode-toggle"
-
-
+import { useEffect, useRef } from 'react';
 
 
 /*
 left to implement (for initial functionality):
 -testing and fine tuning
 -improving mobile design and functionality (utilize tailwind breakpoints)
+-fixing scrollbar to bottom
 
 future
 -adding in more 'tools' for the ai
@@ -29,13 +29,24 @@ future
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
-
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if(messages.length>0)
+      {
+      const element=chatContainerRef.current
+        if(element != null)
+        {
+          element.scrollTop = element.scrollHeight;  
+        }
+    }
+  }, [messages]);
 
   return (
-    <div className="flex flex-col m-8 w-11/12-md py-12 ">
+    <div className="flex flex-col mx-8 w-11/12-md py-12 h-screen text-sm md:text-lg">
       <div className="flex absolute top-0 right-0 m-8">
         <ModeToggle />
       </div>
+      <div ref={chatContainerRef} className='overflow-y-scroll h-9/10'>
       {messages.length<1 ?         
       <Card>
           <CardHeader>
@@ -57,21 +68,23 @@ export default function Chat() {
           {message.parts.map((part, i) => {
             switch (part.type) {
               case 'text':
-                return <div key={`${message.id}-${i}`} className="py-4"><Card><CardContent>{part.text}</CardContent></Card></div>;
+                return <div key={`${message.id}-${i}`} id={`${message.id}`} className="py-4 m-8"><Card><CardContent>{part.text}</CardContent></Card></div>;
             }
           })}
         </div>
       ))}
-
-      <form onSubmit={handleSubmit}>
+      </div>
+      <div className="flex absolute-bottom bottom-0 m-8 justify-center">
+      <form onSubmit={handleSubmit} className='w-full'>
         <input
-          className="fixed bottom-0 p-6 m-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl w-11/12"
+          className="p-6 m-4 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl w-full text-sm md:text-lg"
           value={input}
           placeholder="Ask me anything regarding Ethan Wiegert's background and work."
           onChange={handleInputChange}
           maxLength={200}
         />
       </form>
+      </div>
     </div>
   );
 }
